32982ebe590dc9e4ceda0146ca05280e66b429aa
Craig Cook <craigcook@gmail.com>
2014-09-23 16:19:13 -0700

    Add Mozillian stories, form updates

--- media/js/mozorg/contribute-2015.js
+++ media/js/mozorg/contribute-2015.js
@@ -5,34 +5,10 @@
 ;(function($) {
     'use strict';
 
-    $('#inquiry-form .option input:checked').parents('label').addClass('selected');
-
-    $('#inquiry-form .option input').on('change', function() {
-        $('#inquiry-form .option label').removeClass('selected');
-        $(this).parents('label').addClass('selected');
-    });
-
-    $('#inquiry-form .option input').on('focus', function() {
-        $('#inquiry-form .option label').removeClass('hover');
-        $(this).parents('label').addClass('hover');
-    });
-
-    // Disable placeholder links for prototyping
-    $('a[href="#"]').on('click', function(e) {
-        return false;
-    });
-
-    $('.cta-other a').on('click', function(e) {
-        e.preventDefault();
-        $('.cta-other').fadeOut('fast', function() {
-            $('#other').slideDown();
-        });
-    });
-
-
+    // Add a more/less toggle on story pages
     if ($('.story-more').length > 0) {
         var $more_toggle = $('<div class="more-toggle"><button>' + window.trans('more') + '</button></div>');
-        $more_toggle.insertBefore('.story-more');
+        $more_toggle.insertAfter('.story-more');
 
         $('.more-toggle button').on('click', function() {
            $('.story-more').slideToggle('fast', function() {
@@ -45,6 +21,7 @@
         });
     }
 
+    // Play videos in a modal
     $('a.video-play').attr('role', 'button').on('click', function(e) {
         e.preventDefault();
 
@@ -59,21 +36,50 @@
         });
     });
 
+    // Give the modal a chance to open before playing
     var play_video = function() {
-        // give the modal a chance to open before playing
         setTimeout(function() {
             $('#modal video:first')[0].play();
         }, 400);
     };
 
+    // Style selected option, unstyle previously selected
+    $('#inquiry-form .option input').on('change', function() {
+        $('#inquiry-form .option label').removeClass('selected');
+        $(this).parents('label').addClass('selected');
+    });
+
+    // Style option labels when the option gets focus
+    $('#inquiry-form .option input').on('focus', function() {
+        $('#inquiry-form .option label').removeClass('hover');
+        $(this).parents('label').addClass('hover');
+    });
+
+    // Show 'other ways to contribute' block on thankyou page
+    $('.cta-other a').on('click', function(e) {
+        e.preventDefault();
+        $('.cta-other').fadeOut('fast', function() {
+            $('#other').slideDown();
+        });
+    });
+
+    // Style the selected option if one is selected when the page loads
+    $('#inquiry-form .option input:checked').parents('label').addClass('selected');
+
     $('#inquiry-form input[name="category"]').on('change', function(){
         var $this = $(this);
-        // Get the area IDs
+
+        // Style the selected option (reset all of them first to unstyle previous selection)
+        $('#inquiry-form .option label').removeClass('selected');
+        $this.parents('label').addClass('selected');
+
+        // Get all the area IDs
         var areas = $('#inquiry-form .area').map(function(index){ return this.id; });
+        // Get the area for the selected category
         var categoryarea = 'area-' + $this.attr('value');
 
+        // Show the followup question for categories that have one
         if ($.inArray(categoryarea, areas) !== -1) {
-
             if ($('.areas').is(':hidden')) {
                 $('.areas').slideDown('fast', function(){
                     show_area(categoryarea);
@@ -81,30 +87,30 @@
             } else {
                 show_area(categoryarea);
             }
-
         } else {
             $('.areas, .area').slideUp('fast', function(){
                 $('.area:visible').find('select').prop('selectedIndex', 0);
             });
         }
-
     });
 
     var show_area = function(categoryarea) {
+        // Get the ID of the previously selected area
         var oldarea = $('.area:visible').attr('id');
 
         if ($('#'+oldarea).length > 0) {
             $('#'+oldarea).fadeOut('fast', function(){
                 $('#'+categoryarea).fadeIn('fast', function(){
-                    $(this).find('select').focus();
+                    $(this).find('select').attr('required', true).focus();
                 });
-                $('#'+oldarea).find('select').prop('selectedIndex', 0);
+                $('#'+oldarea).find('select').prop('selectedIndex', 0).attr('required', false);
             });
         } else {
             $('#'+categoryarea).fadeIn('fast');
         }
     };
 
+    // Fake focus styles for styled selects
     $('.select select').on('focus', function(){
         $(this).parent('.select').addClass('focus');
     });
@@ -116,13 +122,14 @@
     // Info tooltips
     $('#inquiry-form .info').on('mouseenter focus', function() {
         var $this = $(this);
-        var target = $(this).attr('href').replace( /.*?(#.*)/g, "$1" ); ;// Extract the target element's ID from the link's href.
+        // Get the target element's ID from the link's href.
+        var target = $(this).attr('href').replace( /.*?(#.*)/g, "$1" );
         $('<div class="tooltip arrow-top">'+ $(target + ' p').text() +'</div>').insertAfter($this).fadeIn('fast');
     });
 
     $('#inquiry-form .info').on('mouseleave blur', function() {
         var $this = $(this);
-        var tooltip = $this.parent('.option').find('.tooltip');
+        var tooltip = $this.parents('.option').find('.tooltip');
         tooltip.delay(100).fadeOut('fast', function() {
             tooltip.remove();
         });
@@ -134,7 +141,7 @@
 
 
 
-/* // Might need this later...
+/* // Info modal with navigation. Might need this later...
 
     var ltr = document.dir === 'ltr';
 
