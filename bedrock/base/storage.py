e369652ec6aa70617b5521063bc0756ec8cffb55
Paul McLanahan <pmac@mozilla.com>
2015-09-30 22:24:34 -0400

    Symlink media files to hashed versions.

--- bedrock/base/storage.py
+++ bedrock/base/storage.py
@@ -4,11 +4,28 @@
 
 # This is here so that we can mix storages as we need. We'll do this for pipeline
 # and whitenoise with hashing and gzip when we get those both working.
+import os
 
 from pipeline.storage import PipelineMixin
-# This part of whitenoise is Python 2.6 compatible
 from whitenoise.django import GzipManifestStaticFilesStorage
 
 
 class ManifestPipelineStorage(PipelineMixin, GzipManifestStaticFilesStorage):
-    pass
+    def post_process(self, paths, dry_run=False, **options):
+        files = super(ManifestPipelineStorage, self).post_process(paths, dry_run, **options)
+        to_link = {}
+
+        for name, hashed_name, processed in files:
+            if name != hashed_name:
+                to_link[name] = hashed_name
+
+            yield name, hashed_name, processed
+
+        if dry_run:
+            return
+
+        for name, hashed_name in to_link.iteritems():
+            name_path = self.path(name)
+            os.unlink(name_path)
+            os.symlink(self.path(hashed_name), name_path)
+            yield name, hashed_name, True
